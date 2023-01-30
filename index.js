a186a165e137fae5466a437175c96974c14038e1
rajsharma333 <raj.sharma333@gmail.com>
2023-01-30 20:15:42 +0530

    husky setup test

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-var a = 4
+var a = 5
 root.render('Kunzum');
